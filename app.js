22a845186013bc205814a697aea00059c736dbd9
krish-98 <muralikrishnanm98@gmail.com>
2021-10-02 16:27:45 +0530

    minor tweaks

--- app.js
+++ app.js
@@ -2,32 +2,37 @@ const billAmount = document.querySelector("#bill-amount")
 const cashGiven = document.querySelector("#cash-given")
 const checkButton = document.querySelector("#check-button")
 const errorMessage = document.querySelector("#error-message")
+const showChange = document.querySelector("#show-change")
 const noOfNotes = document.querySelectorAll('.no-of-notes')
 
 const notes = [2000, 500, 100, 50, 20, 10, 1]
 
 checkButton.addEventListener("click", () => {
-  hideMessage()
-  if (billAmount.value > 0 && billAmount.value.length > 0) { 
+  validateUserInput() 
+})
+
+const validateUserInput = () => {
+    hideMessage()
+    if (billAmount.value > 0 && billAmount.value.length > 0) { 
     console.log(billAmount.value.length)
     if (cashGiven.value >= billAmount.value) { 
       let amountToBeReturned = cashGiven.value - billAmount.value 
+      showChange.innerText = amountToBeReturned
       calculateChange(amountToBeReturned)
     } else {
-      showErrorMessage("Do you wanna hand-grind some rice in our Kitchen?")
+      showErrorMessage("The amount given should be equivalent or greater than the bill amount ")
     } 
   } else {
     showErrorMessage("Enter a valid number")
   }
-})
+}
 
 const hideMessage = () => {
   errorMessage.style.display = "none"
 }
 
 const calculateChange = amountToBeReturned => {
-  // go over all the available
-  for( let i = 0; i<notes.length; i++) {
+  for( let i = 0; i < notes.length; i++) {
     // no of notes need for the denomination
     const numberOfNotes = Math.trunc(amountToBeReturned / notes[i])
 
@@ -35,9 +40,7 @@ const calculateChange = amountToBeReturned => {
     amountToBeReturned = amountToBeReturned % notes[i]
 
     noOfNotes[i].innerText = numberOfNotes
-    console.log(numberOfNotes)
-  }
-  
+  } 
 }
 
 const showErrorMessage = message => {
